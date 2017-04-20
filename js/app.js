c5350f4b184de75bcb5a4465fc36c242326a68c2
kevin <kevinpuyol@live.fr>
2017-04-20 15:36:53 +0200

    parties exo1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		message: 'Hello world!',
+		hendek:'<p>Attention aux hendeks</p>',
+		'items': ['Michmich', 'Jeanmi', 'Jacob','Jackie','Jacobo'],
+	}
+
+
+})
